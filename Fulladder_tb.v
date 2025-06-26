3643df94a348e20c173aaf444522bf17c9cbbf89
SayaliMahajan14 <sayalimahajan85@gmail.com>
2025-06-27 00:02:54 +0530

    Add files via upload

---
+++ Fulladder_tb.v
@@ -0,0 +1,19 @@
+module Fulladder_tb;
+reg a,b,cin;
+wire sum,cout;
+
+Fulladder instance1( .a(a), .b(b), .cin(cin), .sum(sum), .cout(cout));
+
+initial begin
+   
+   a = 0; b = 0; cin = 0; 
+   #10 a = 0; b = 0; cin = 1; 
+   #10 a = 0; b = 1; cin = 0; 
+   #10 a = 0; b = 1; cin = 1; 
+   #10 a = 1; b = 0; cin = 0; 
+   #10 a = 1; b = 0; cin = 1; 
+   #10 a = 1; b = 1; cin = 0; 
+   #10 a = 1; b = 1; cin = 1; 
+
+  end
+endmodule
